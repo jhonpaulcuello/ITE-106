73bfcf3ec66d5274e49d300d0fbe86851cc418f2
jhonpaulcuello <jhonpaulcuello947@gmail.com>
2024-10-04 14:57:07 +0800

    Add files via upload

---
+++ Lamp2.java
@@ -0,0 +1,12 @@
+class Lamp{
+	boolean isOn;
+	
+	void turnon() {
+		isOn = true;
+		System.out.println("Light On? " + isOn);
+	}
+	void turnOff(){
+		isOn = false;
+		System.out.println("Light On? " +isOn);
+	}
+}
